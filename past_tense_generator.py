8662032ea3e295df583e2d64b3c79b725eee8d0c
Kaitlynn Hetzel <knh33@pitt.edu>
2016-07-01 01:45:27 -0400

    added in exceptions for double-consonant needing verbs (tap -> tapped)

--- past_tense_generator.py
+++ past_tense_generator.py
@@ -2,10 +2,37 @@
 
 #these verbs are ones with irregular past tenses 
 irregular_forms =  {"go": "went","buy": "bought", "break": "broke", "sit": "sat", "come":"came", "eat": "ate", "sleep": "slept", "see": "saw", "pay": "paid", "sing": "sang", "tell": "told", "get": "got", "teach": "taught", "feel": "felt",
-"hear": "heard"}
+"hear": "heard", "understand": "understood"}
 
 #these verbs are the same in both the present and pass tense 
 single_forms =  ["cut", "put", "let", "hurt", "quit", "read", "broadcast", "hit", "cost", "spread"]
+
+#a vowel list, for reference
+vowels = ['a','e','i','o','u','y']
+
+#suffixes of root words that will never be repeated
+end_hushers = ["x", "lk", "sh", "ch", "ck", "h", "k", "nt", "lp"]
+def needsDoubleConsonant(verb):
+    count = 0
+    for letter in verb:
+        if letter in vowels:
+            count = count + 1
+
+    if count > 1:
+        return False
+    else:
+        last_two = verb[len(verb)-2] + verb[len(verb)-1]
+        last = verb[len(verb)-1]
+        #if the last letter is one that will never be repeated
+        if last in end_hushers:
+            return False
+        #if the suffix of the word will never need a repeated letter
+        elif last_two in end_hushers:
+            return False
+        #if the last two letters of the verb are already a repeated letter
+        elif last == verb[len(verb)-2] :
+            return False
+        return True
 
 #repeat until user is finished
 while True:
@@ -26,7 +53,7 @@ while True:
         print(to_past)
     #checking special case if verb ends in a y
     elif to_past[len(to_past) - 1] == 'y':
-        vowels = ['a','e','i','o','u','y']
+        
         if to_past[len(to_past) - 2] in vowels:
             to_past = to_past + "ed"
             print (to_past)
@@ -36,7 +63,11 @@ while True:
             to_past = "".join(temp)
             to_past = to_past + "ed"
             print (to_past)
+    elif needsDoubleConsonant(to_past):
+        to_past = to_past + to_past[len(to_past) -1] + "ed"
+        print (to_past)
     #typical case - just add "ed"       
     else:
         to_past = to_past + "ed"
         print (to_past)
+
